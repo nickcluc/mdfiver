8257a1b7f3c546973d94ca73534951875bef9512
Nick Clucas <nick.clucas@drugdev.com>
2016-04-12 13:11:15 -0400

    Added output msg

--- mdfiver.rb
+++ mdfiver.rb
@@ -5,3 +5,5 @@ output_path = File.dirname(input_file_path)
 filename = File.basename(input_file_path).split('.').first
 
 File.open(File.join(output_path, filename) + '.MD5', 'w') { |f| f.write Digest::MD5.hexdigest File.read(input_file_path)}
+
+puts "MD5 file created @ #{File.join(output_path, filename) + '.MD5'}"
